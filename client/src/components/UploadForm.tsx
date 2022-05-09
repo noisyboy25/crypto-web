8a345fc4f3f21f05412467a4b53c4c6160bfb549
noisyboy25 <dj402and@gmail.com>
2022-05-09 19:47:30 +0300

    Change file encryption steps

--- client/src/components/UploadForm.tsx
+++ client/src/components/UploadForm.tsx
@@ -11,19 +11,43 @@ import {
   faFileArrowUp,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 export const UploadForm = () => {
-  const [url, setUrl] = useState('');
+  const [fileUrl, setFileUrl] = useState('');
+  const [keyFileUrl, setKeyFileUrl] = useState('');
+
   const fileName = useRef('');
-  const link = useRef<HTMLAnchorElement>(null);
+  const keyFileName = useRef('');
+
+  const fileLink = useRef<HTMLAnchorElement>(null);
+  const keyFileLink = useRef<HTMLAnchorElement>(null);
 
   const { register, handleSubmit, reset } = useForm();
 
+  const getKey = async () => {
+    const res = await fetch('/api/key');
+
+    if (!res.ok) return;
+
+    const blob = await res.blob();
+    const blobUrl = URL.createObjectURL(blob);
+    setKeyFileUrl(blobUrl);
+
+    const disposition = res.headers.get('Content-Disposition');
+    keyFileName.current = disposition?.split(';')[1].split('=')[1] || '';
+  };
+  useEffect(() => {
+    if (keyFileUrl) keyFileLink.current!.click();
+  }, [keyFileUrl]);
+
   const uploadFiles = async (data: any, mode: 'enc' | 'dec' = 'enc') => {
     const formData = new FormData();
-    formData.append('file', data);
+    console.log(data);
+
+    formData.append('file', data.file[0]);
+    formData.append('keyFile', data.keyFile[0]);
 
     const res = await fetch(`/api/${mode}`, {
       method: 'POST',
@@ -33,34 +57,51 @@ export const UploadForm = () => {
     if (!res.ok) return;
 
     const blob = await res.blob();
-    const blobUrl = URL.createObjectURL(blob);
-    setUrl(blobUrl);
-    fileName.current = mode === 'enc' ? 'file.bin' : 'file.txt';
+    const blobUrl = `${URL.createObjectURL(blob)}`;
+    const disposition = res.headers.get('Content-Disposition');
+    fileName.current = decodeURIComponent(
+      disposition?.split(';')[1].split('=')[1] || ''
+    );
+
+    console.log(fileName.current);
+
+    setFileUrl(blobUrl);
     console.log(blob);
   };
 
-  const downloadBlob = useCallback(async () => {
-    if (url) link.current?.click();
-  }, [url]);
+  const downloadBlob = async () => {
+    if (fileUrl) fileLink.current!.click();
+  };
 
   useEffect(() => {
-    downloadBlob();
+    if (fileUrl) fileLink.current!.click();
     reset({ file: null });
     console.log('reset');
-  }, [url, reset, downloadBlob]);
+  }, [fileUrl, reset]);
 
   const onEncrypt = (data: any) => {
-    uploadFiles(data.file[0], 'enc');
-    console.log(data.file[0]);
+    console.log(data);
+    uploadFiles(data, 'enc');
   };
 
   const onDecrypt = (data: any) => {
-    uploadFiles(data.file[0], 'dec');
-    console.log(data.file[0]);
+    console.log(data);
+    uploadFiles(data, 'dec');
   };
 
   return (
     <Flex flexDirection="column" gap={4}>
+      <ButtonGroup>
+        <Button
+          onClick={getKey}
+          leftIcon={<FontAwesomeIcon icon={faCloudDownload} />}
+        >
+          Generate key
+        </Button>
+      </ButtonGroup>
+      <FormControl>
+        <Input isRequired type="file" accept=".crk" {...register('keyFile')} />
+      </FormControl>
       <FormControl>
         <Input isRequired type="file" {...register('file')} />
       </FormControl>
@@ -80,18 +121,28 @@ export const UploadForm = () => {
       </ButtonGroup>
       <ButtonGroup>
         <Button
+          overflow="hidden"
+          justifyContent="flex-start"
           onClick={downloadBlob}
-          isDisabled={!url}
+          isDisabled={!fileUrl}
           leftIcon={<FontAwesomeIcon icon={faCloudDownload} />}
         >
           Download {fileName.current}
         </Button>
       </ButtonGroup>
       <a
+        href={keyFileUrl}
         style={{ display: 'none' }}
-        href={url}
+        download={keyFileName.current}
+        ref={keyFileLink}
+      >
+        Download key
+      </a>
+      <a
+        style={{ display: 'none' }}
+        href={fileUrl}
         download={fileName.current}
-        ref={link}
+        ref={fileLink}
       >
         Download
       </a>
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,32 +5,59 @@ import fileUpload, { UploadedFile } from 'express-fileupload';
 import { Readable } from 'stream';
 import Aes256 from '../../lib/aes256';
 import { pipeline } from 'stream';
+import compression from 'compression';
 
 const PORT = process.env.PORT || 5000;
 
-let key = 'secret';
-key = crypto.createHash('sha256').update(key).digest('base64').substring(0, 32);
+let secret = 'secret';
 
-const iv = Aes256.randIV();
+const parseKey = (text: string) => {
+  const parsed = JSON.parse(text);
+  return { iv: Buffer.from(parsed.iv, 'base64'), key: parsed.key };
+};
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(fileUpload());
+app.use(compression());
 
 app.get('/test', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/api/key', (req, res) => {
-  res.json({ iv });
+  const iv = Aes256.randIV();
+  const key = crypto
+    .createHash('sha256')
+    .update(secret)
+    .digest('base64')
+    .substring(0, 32);
+
+  res
+    .setHeader('Content-Disposition', `attachment; filename=${Date.now()}.crk`)
+    .send(JSON.stringify({ iv: iv.toString('base64'), key }));
 });
 
 app.post('/api/enc', (req, res) => {
   if (req.files) {
-    console.log(req.files.file);
+    console.log(req.files);
+
+    if (!(req.files.keyFile && req.files.file)) return res.sendStatus(400);
+    const file = <UploadedFile>req.files.file;
+    const keyFile = <UploadedFile>req.files.keyFile;
+
+    const { iv, key } = parseKey(keyFile.data.toString());
+
+    console.log({ iv, key });
 
     const sha256 = new Aes256(iv);
+
+    console.log(`attachment; filename=${encodeURIComponent(file.name)}.crb`);
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename=${encodeURIComponent(file.name)}.crb`
+    );
 
     pipeline(
       Readable.from((req.files.file as UploadedFile).data),
@@ -47,16 +74,27 @@ app.post('/api/dec', (req, res) => {
   if (req.files) {
     console.log(req.files.file);
 
+    if (!(req.files.keyFile && req.files.file)) return res.sendStatus(400);
+
+    const file = <UploadedFile>req.files.file;
+    const keyFile = <UploadedFile>req.files.keyFile;
+
+    const { iv, key } = parseKey(keyFile.data.toString());
+
     const sha256 = new Aes256(iv);
 
-    pipeline(
-      Readable.from((req.files.file as UploadedFile).data),
-      sha256.decrypt(key),
-      res,
-      (err) => {
-        if (err) console.log(err);
-      }
+    console.log(`attachment; filename=${file.name}.crb`);
+
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename=${encodeURIComponent(
+        file.name.replace(/.crb$/, '')
+      )}`
     );
+
+    pipeline(Readable.from(file.data), sha256.decrypt(key), res, (err) => {
+      if (err) console.log(err);
+    });
   }
 });
 
