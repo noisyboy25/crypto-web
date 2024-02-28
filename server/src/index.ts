c3c94dfc726b8301c1c944cbeb9b75894b5a151a
noisyboy25 <dj402and@gmail.com>
2024-02-28 17:24:13 +0300

    Add file download

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,19 @@
 import crypto from 'crypto';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import Aes256 from '../../lib/aes256';
 import { pipeline, Stream } from 'stream';
 import compression from 'compression';
 import fs from 'fs';
 import busboy from 'busboy';
+import path from 'path';
 
 const PORT = process.env.PORT || 5000;
 
 let secret = 'secret';
 
 const TMP_DIR = process.env.TMP_DIR || '../tmp';
+const TMP_PATH = path.join(__dirname, TMP_DIR);
 
 const streamToString = (stream: Stream) => {
   const chunks: Buffer[] = [];
@@ -30,10 +32,82 @@ const parseKey = (text: string) => {
 const app = express();
 
 app.use(morgan('dev'));
+app.use('/tmp', express.static(TMP_PATH));
 app.use(compression());
 
-app.get('/test', (req, res) => {
-  res.send('Hello World');
+const processFile = (
+  req: Request,
+  res: Response,
+  mode: 'enc' | 'dec' = 'enc'
+) => {
+  const bb = busboy({
+    headers: {
+      'content-type': (req.headers['Content-Type'] ||
+        req.headers['content-type']) as string,
+    },
+  });
+
+  req.pipe(bb);
+
+  let parsedKey: { key: string; iv: Buffer };
+
+  let filename = '';
+
+  bb.on('file', (name, file, info: any) => {
+    console.log(info);
+
+    if (name === 'keyFile') {
+      streamToString(file).then((text) => {
+        parsedKey = parseKey(text);
+      });
+
+      return;
+    }
+
+    const run = () => {
+      if (!parsedKey) {
+        setTimeout(() => run(), 500);
+      } else {
+        const aes256 = new Aes256(parsedKey.iv);
+
+        if (!fs.existsSync(TMP_PATH)) fs.mkdirSync(TMP_PATH);
+        filename = info.filename;
+        let filePath = '';
+        if (mode === 'enc') {
+          filename = `${filename}.crb`;
+          filePath = path.join(TMP_PATH, filename);
+        } else if (mode === 'dec') {
+          filename = filename.replace(/.crb$/, '');
+          filePath = path.join(TMP_PATH, filename);
+        }
+        const writeable = fs.createWriteStream(filePath);
+
+        pipeline(
+          file,
+          mode === 'enc'
+            ? aes256.encrypt(parsedKey.key)
+            : aes256.decrypt(parsedKey.key),
+          writeable,
+          (err) => {
+            if (err) return console.log(err);
+          }
+        );
+      }
+    };
+    run();
+  });
+  bb.on('close', () => {
+    console.log(filename);
+    res.status(200).json({ filename });
+  });
+};
+
+app.get('/tmp/:filename', (req, res) => {
+  const { filename } = req.params;
+
+  if (!fs.existsSync(path.join(TMP_PATH, filename))) return res.sendStatus(404);
+
+  res.sendFile(path.join(TMP_PATH, filename));
 });
 
 app.get('/api/key', (req, res) => {
@@ -49,124 +123,8 @@ app.get('/api/key', (req, res) => {
     .send(JSON.stringify({ iv: iv.toString('base64'), key }));
 });
 
-app.post('/api/enc', (req, res) => {
-  const bb = busboy({
-    headers: {
-      'content-type': (req.headers['Content-Type'] ||
-        req.headers['content-type']) as string,
-    },
-  });
+app.post('/api/enc', (req, res) => processFile(req, res, 'enc'));
 
-  req.pipe(bb);
-
-  let parsedKey: { key: string; iv: Buffer };
-
-  bb.on('file', (name, file, info: any) => {
-    console.log(info);
-
-    if (name === 'keyFile') {
-      streamToString(file).then((text) => {
-        parsedKey = parseKey(text);
-      });
-
-      return;
-    }
-
-    const encryptFile = () => {
-      if (!parsedKey) {
-        setTimeout(() => encryptFile(), 500);
-      } else {
-        const aes256 = new Aes256(parsedKey.iv);
-
-        console.log(
-          `attachment; filename=${encodeURIComponent(info.filename)}.crb`
-        );
-        res.setHeader(
-          'Content-Disposition',
-          `attachment; filename=${encodeURIComponent(info.filename)}.crb`
-        );
-
-        const writeable = fs.createWriteStream(`${info.filename}.crb`);
-
-        pipeline(
-          file,
-          aes256.encrypt(parsedKey.key),
-          writeable,
-          // res,
-          (err) => {
-            if (err) return console.log(err);
-            res.sendStatus(200);
-          }
-        );
-      }
-    };
-    encryptFile();
-  });
-
-  bb.on('finish', () => res.status(200));
-});
-
-app.post('/api/dec', (req, res) => {
-  const bb = busboy({
-    headers: {
-      'content-type': (req.headers['Content-Type'] ||
-        req.headers['content-type']) as string,
-    },
-  });
-
-  req.pipe(bb);
-
-  let parsedKey: { key: string; iv: Buffer };
-
-  bb.on('file', (name, file, info: any) => {
-    console.log(info);
-
-    if (name === 'keyFile') {
-      streamToString(file).then((text) => {
-        parsedKey = parseKey(text);
-      });
-
-      return;
-    }
-
-    const decryptFile = () => {
-      if (!parsedKey) {
-        setTimeout(() => decryptFile(), 500);
-      } else {
-        const aes256 = new Aes256(parsedKey.iv);
-
-        console.log(
-          `attachment; filename=${encodeURIComponent(
-            info.filename.replace(/.crb$/, '')
-          )}`
-        );
-        res.setHeader(
-          'Content-Disposition',
-          `attachment; filename=${encodeURIComponent(
-            info.filename.replace(/.crb$/, '')
-          )}`
-        );
-
-        const writeable = fs.createWriteStream(
-          `${info.filename.replace(/.crb$/, '')}`
-        );
-
-        pipeline(
-          file,
-          aes256.decrypt(parsedKey.key),
-          writeable,
-          // res,
-          (err) => {
-            if (err) return console.log(err);
-            res.sendStatus(200);
-          }
-        );
-      }
-    };
-    decryptFile();
-  });
-
-  bb.on('finish', () => res.status(200));
-});
+app.post('/api/dec', (req, res) => processFile(req, res, 'dec'));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
